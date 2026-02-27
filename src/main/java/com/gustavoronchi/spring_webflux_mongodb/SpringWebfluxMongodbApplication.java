4520eb977f2fe1e5c3f73ca3110289eedea6ed42
Gustavo <gustavoirl456@outlook.com>
2026-02-27 16:12:43 -0300

    Projeto criado

---
+++ src/main/java/com/gustavoronchi/spring_webflux_mongodb/SpringWebfluxMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.spring_webflux_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoronchi/spring_webflux_mongodb/SpringWebfluxMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.spring_webflux_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
